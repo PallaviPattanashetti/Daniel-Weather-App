64771cc61dce2876cbe5ebce12f8310764a09f4d
PallaviPattanashetti <Pallavi.pattanashetti@gmail.com>
2025-12-16 09:51:39 -0800

    added Id

--- script/app.js
+++ script/app.js
@@ -9,11 +9,15 @@ getLocationBtn
 
 
 
- function getCurrentPosition(){
+ function apiCall(){
 
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=stockton&appid=62042061ec88239bfcf89d1be981c431')
     .then(response => response.json())
     .then(json => console.log(json))
  }
+apiCall();
+
+
+
 
  getCurrentPosition()
---
+++ script/environment.js
@@ -0,0 +1,2 @@
+const APIKEY  = "62042061ec88239bfcf89d1be981c431";
+export { APIKEY }   
