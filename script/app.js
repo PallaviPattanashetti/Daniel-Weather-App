c9508ca61831150d4034a08281d1bac903a25e65
PallaviPattanashetti <Pallavi.pattanashetti@gmail.com>
2025-12-16 12:57:19 -0800

    working on fav cards

--- script/app.js
+++ script/app.js
@@ -1,22 +1,29 @@
-//const getLongitude = "/longitude";
-//const getLatitude = "Latitude";
-const searchUserInput= document.getElementById("searchUserInput");
+const getLongitude = "/longitude";
+const getLatitude = "Latitude";
+const searchUserInput = document.getElementById("searchUserInput");
 const getLocationBtn = document.getElementById("getLocationBtn");
-const darkModeToggleLink= document.getElementById("darkModeToggleLink");
+const darkModeToggleLink = document.getElementById("darkModeToggleLink");
 const favouriteStar = document.getElementById("favouriteStar");
+const getCurrentLocation = document.getElementById("getCurrentLocation");
+const cityName = document.getElementById("cityName");
+const dayOfWeek = document.getElementById("dayOfWeek");
+const tempRecord = document.getElementById("tempRecord");
+const weatherIcon = document.getElementById("weatherIcon");
 
 
 
 
- function apiCall(){
+function apiCall() {
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=stockton&appid=62042061ec88239bfcf89d1be981c431')
-    .then(response => response.json())
-    .then(json => console.log(json))
- }
+   fetch('https://api.openweathermap.org/data/2.5/forecast?q=stockton&appid=62042061ec88239bfcf89d1be981c431')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
 apiCall();
 
-getCurrentPosition();
+
+
+
 
 
 
